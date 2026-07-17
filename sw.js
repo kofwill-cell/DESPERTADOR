46ec0aeae6eedeeece3a6e8336e13408d409078f
kofwill-cell <kofwill@gmail.com>
2026-07-17 02:19:38 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service worker básico: permite instalar como app y abre sin conexión.
-const CACHE = "couch-v9";
+const CACHE = "couch-v11";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
